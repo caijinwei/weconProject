f861899d7a24acbc3b4deb89014c9ad676c79a67
caijinwei <429829114@qq.com>
2017-07-27 22:42:08 +0800

    1.实时数据显示页面轮询没完成2.mosquitto消息推送完成

--- Server/box/box-impl/src/main/java/com/wecon/box/action/TestAction.java
+++ Server/box/box-impl/src/main/java/com/wecon/box/action/TestAction.java
@@ -1,17 +1,18 @@
 package com.wecon.box.action;
 
 import com.alibaba.fastjson.JSONObject;
-import com.sun.javafx.scene.control.skin.VirtualFlow;
 import com.wecon.box.api.RedisPiBoxApi;
 import com.wecon.box.entity.PiBoxCom;
 import com.wecon.box.entity.PiBoxComAddr;
-import com.wecon.box.entity.RedisPiBoxActData;
-import com.wecon.box.param.StcOperateData;
 import com.wecon.restful.annotation.WebApi;
 import com.wecon.restful.core.Output;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Description;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -64,6 +65,69 @@ public class TestAction {
     }
 
 
+    /*
+    * 这边写   根据机器码获取redis里面的数据
+    * */
+    @Description("根据获取的机器码向redis服务器请求数据")
+    @RequestMapping(value = "/getActDate")
+    public Output getActDate(@RequestParam("machine_code")String machine_code){
+        /*RedisPiBoxActData model = new RedisPiBoxActData();
+        model.machine_code = "1001";
+        model.time = "2017-07-26 10:20:11";
+        model.act_time_data_list = new ArrayList<PiBoxCom>();
+        PiBoxCom com=new PiBoxCom();
+        com.com="1";
+        com.addr_list=new ArrayList<PiBoxComAddr>();
+        PiBoxComAddr addr=new PiBoxComAddr();
+        addr.addr="10";
+        addr.value="0.256";
+        com.addr_list.add(addr);
+        model.act_time_data_list.add(com);
+        redisPiBoxApi.saveRedisPiBoxActData(model);*/
+
+        JSONObject data = new JSONObject();
+        data.put("piBoxActDateMode", redisPiBoxApi.getRedisPiBoxActData(machine_code));
+        return new Output(data);
+    }
 
 
+    /*
+    * 这边写mosquito的发送请求
+    *
+    * */
+    @Description("mosquito消息的推送测试")
+    @RequestMapping(value="/putMess")
+    public Output putMQTTMess(@RequestParam("machine_code")String machine_code,@RequestParam("com")String com,@RequestParam("addr")String addr,@RequestParam("value")String value) throws MqttException {
+        ServerMqtt server = new ServerMqtt();
+        server.message = new MqttMessage();
+        server.message.setQos(1);
+        server.message.setRetained(true);
+
+        PiBoxComAddr addr1=new PiBoxComAddr();
+        addr1.addr=addr;
+        addr1.addr_id=addr;
+        addr1.value=value;
+        List<PiBoxCom> operate_data_list = new ArrayList<PiBoxCom>();
+        PiBoxCom piBoxCom=new PiBoxCom();
+        List<PiBoxComAddr> piBoxComAddrs=new ArrayList<PiBoxComAddr>();
+        piBoxCom.com=com;
+        piBoxComAddrs.add(addr1);
+        piBoxCom.addr_list= piBoxComAddrs;
+        operate_data_list.add(piBoxCom);
+
+
+        JSONObject jsonObject =new JSONObject();
+        jsonObject.put("act",2000);
+        jsonObject.put("machine_code",machine_code);
+        jsonObject.put("data",operate_data_list);
+        jsonObject.put("feedback",0);
+        String message=jsonObject.toJSONString();
+        System.out.println(message);
+        server.message.setPayload((message).getBytes());
+        server.topic11 = server.client.getTopic("pibox/stc"+machine_code);
+
+        server.publish(server.topic11 , server.message);
+        System.out.println(server.message.isRetained() + "------ratained状态");
+        return new Output();
+    }
 }
