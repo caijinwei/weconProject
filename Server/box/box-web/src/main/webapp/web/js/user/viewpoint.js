f2dbee7b723fea3d4f48b519d074a894b34f0610
caijinwei <429829114@qq.com>
2017-08-14 10:09:40 +0800

    1.提交前的BUG修改

--- Server/box/box-web/src/main/webapp/web/js/user/viewpoint.js
+++ Server/box/box-web/src/main/webapp/web/js/user/viewpoint.js
@@ -12,11 +12,35 @@ appModule.controller("listController", function ($scope, $http, $compile) {
         } else if (type == 1) {
             $scope.title = "历史监控点权限列表";
         }
+        //展示主页面列表
         $scope.showRealPoint(type, viewid);
+        //第一次展示模态窗口
+        /*
+         * 分页
+         * 分配实时历史监控点
+         *       展示管理员用户余下监控点
+         *
+         * */
+        $scope.showRestPoint = function () {
+            $scope.paginationConf = {
+                currentPage: 1,
+                itemsPerPage: 10,
+                totalItems: $scope.count,
+                pagesLength: 15,
+                perPageOptions: [5, 10, 20, 50, 100],
+                rememberPerPage: 'perPageItems',
+                onChange: function () {
+                    if (this.currentPage != 0) {
+                        $scope.getRestList(this.currentPage, this.itemsPerPage);
+                    }
+                }
+            }
+        }
     }
 
 
     $scope.showRealPoint = function (type, view_id) {
+
         var params =
         {
             view_id: view_id,
@@ -35,27 +59,7 @@ appModule.controller("listController", function ($scope, $http, $compile) {
         });
     }
 
-    /*
-     * 分页
-     * 分配实时历史监控点
-     *       展示管理员用户余下监控点
-     *
-     * */
-    $scope.showRestPoint = function () {
-        $scope.paginationConf = {
-            currentPage: 1,
-            itemsPerPage: 10,
-            totalItems: $scope.count,
-            pagesLength: 15,
-            perPageOptions: [5, 10, 20, 50, 100],
-            rememberPerPage: 'perPageItems',
-            onChange: function () {
-                if (this.currentPage != 0) {
-                    $scope.getRestList(this.currentPage, this.itemsPerPage);
-                }
-            }
-        }
-    }
+
     $scope.paginationConf = {
         totalItems: $scope.count,
     }
@@ -86,24 +90,14 @@ appModule.controller("listController", function ($scope, $http, $compile) {
             alert("ajax error");
         });
     }
+    /*
+     * 添加监控点button的事件
+     * */
+    $scope.showRestList = function () {
+        $scope.$apply();
+        $scope.getRestList($scope.paginationConf.currentPage, $scope.paginationConf.itemsPerPage);
+    }
 
-
-    $scope.selected = [];
-    $scope.isChecked = function (id) {
-        return $scope.selected.indexOf(id) >= 0;
-    };
-    //更新某一列数据的选择
-    $scope.updateSelection = function ($event, id) {
-        var checkbox = $event.target;
-        var action = (checkbox.checked ? 'add' : 'remove');
-        updateSelected(action, id);
-    };
-    var updateSelected = function (action, id) {
-        if (action == 'add' && $scope.selected.indexOf(id) == -1)
-            $scope.selected.push(id);
-        if (action == 'remove' && $scope.selected.indexOf(id) != -1)
-            $scope.selected.splice($scope.selected.indexOf(id), 1);
-    };
     /*
      * 提交选中的监控点
      * */
@@ -113,19 +107,24 @@ appModule.controller("listController", function ($scope, $http, $compile) {
         var chk_value = [];
         $('input[name="cbid"]:checked').each(function () {
             chk_value.push($(this).val());
-
             var tem = "right_" + $(this).val();
             rightOption.push($("input[name=" + tem + "]:checked").val());
         });
         var ids = chk_value.join(",");
         var rights = rightOption.join(",");
+        if(chk_value.length==0)
+        {
+            alert("没有添加任何监控点");
+            $("#showRestOpint").modal("hide");
+            return;
+        }
         var params = {viewId: viewid, selectedId: ids, rights: rights};
         T.common.ajax.request("WeconBox", "Viewpoint/setViewPoint", params, function (data, code, msg) {
             if (code == 200) {
                 $("#showRestOpint").modal("hide");
                 alert("添加成功");
                 var type = T.common.util.getParameter("type");
-                $scope.showRealPoint(type,viewid);
+                $scope.showRealPoint(type, viewid);
                 //$scope.getRestList( $scope.paginationConf.currentPage, $scope.paginationConf.itemsPerPage);
             }
             else {
