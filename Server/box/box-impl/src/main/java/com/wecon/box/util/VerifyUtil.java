08ecfb04da563aa734c05608a2f1ff629c9cf196
caijinwei <429829114@qq.com>
2017-09-15 11:04:11 +0800

    1.检查固件版本更新的验证方法完成

--- Server/box/box-impl/src/main/java/com/wecon/box/util/VerifyUtil.java
+++ Server/box/box-impl/src/main/java/com/wecon/box/util/VerifyUtil.java
@@ -35,4 +35,64 @@ public class VerifyUtil {
         b = m.matches();
         return b;
     }
+
+    /*
+    *
+    * */
+    public static String getVersionNum(String version) {
+//        version = "sdsd2V4.0.0-P:A8 ";
+
+        String result = "";
+        //分割 前面的“-”
+        Integer index = version.indexOf("-");
+        String s = version.substring(0, index);
+        String tempV=version.substring(0, index);
+
+        //提取版本号
+        String REGEX = "[[0-9]+\\.]+[0-9]+";
+        Pattern p = Pattern.compile(REGEX);
+        Matcher m = p.matcher(tempV);
+        while (m.find()) {
+            result = (m.group());
+        }
+        System.out.println(result);
+        return  result;
+    }
+
+    /*
+    * localVsersion 本地版本
+    * serverVsersion 服务器上面的版本
+    *
+    * return
+    *        1)true
+    *        serrverVersion > localVersion
+    *        需要更新
+    *
+    *        2）flase
+    *        serverVersion <= localVersion
+    *        不需要更新
+   * */
+    public static boolean isNewVersion(String localVersion, String serverVersion) {
+        if (localVersion.equals(serverVersion)) {
+            return false;
+        }
+        String[] localArray = localVersion.split("\\.");
+        String[] onlineArray = serverVersion.split("\\.");
+
+        int length = localArray.length < onlineArray.length ? localArray.length : onlineArray.length;
+
+        for (int i = 0; i < length; i++) {
+            if (Integer.parseInt(onlineArray[i]) > Integer.parseInt(localArray[i])) {
+                return true;
+            } else if (Integer.parseInt(onlineArray[i]) < Integer.parseInt(localArray[i])) {
+                return false;
+            }
+            // 相等 比较下一组值
+        }
+
+        return true;
+    }
+    public static void main(String[] args){
+        System.out.println(isNewVersion(getVersionNum("sdsd2V4.0.0-P:A8"),getVersionNum("sdsd2V4.12.0-P:A8")));
+    }
 }
