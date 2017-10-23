d1d2f394d0795ec539cf4a93ff2fa9ec377a416b
zengzhipeng <86779961@qq.com>
2017-10-23 09:31:48 +0800

    hw云配置修改

--- Server/box/box-web/src/main/webapp/web/js/main.js
+++ Server/box/box-web/src/main/webapp/web/js/main.js
@@ -56,7 +56,13 @@ appModule.controller("infoController", function ($scope, $http, $compile) {
     function attachContext(selector,map) {
         context.attach(selector, [{
             text: "地图定位",
-            href: 'web/html/box-map.html?map='+map,
+            href: 'web/html/box-map.html?selAlarm=0&map='+map,
+            target:'content_frame',
+            action: function () {
+            }
+        },{
+            text: "报警设备",
+            href: 'web/html/box-map.html?selAlarm=1&map=' + map,
             target:'content_frame',
             action: function () {
             }
