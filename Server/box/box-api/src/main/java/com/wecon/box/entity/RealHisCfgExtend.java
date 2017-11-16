9d23bc510e581d0bfc38fe79c6353c8ce6366332
wanghp@we-con.com.cn <wanghp@we-con.com.cn>
2017-11-16 15:40:24 +0800

    代码调整

--- Server/box/box-api/src/main/java/com/wecon/box/entity/RealHisCfgExtend.java
+++ Server/box/box-api/src/main/java/com/wecon/box/entity/RealHisCfgExtend.java
@@ -10,6 +10,6 @@ public class RealHisCfgExtend extends RealHisCfgDevice{
 	public String com;
 	public long addr_id;
 	public String upd_time;
-	public static String[] UPDATE_REAL_HIS_FIELD_FILTER = new String[]{"com", "addr_id", "upd_time", "data_type", "data_id", "name", "addr", "addr_type", "digit_count", "data_limit", "his_cycle","rid", "digit_binary"};
+	public static final String[] UPDATE_REAL_HIS_FIELD_FILTER = new String[]{"com", "addr_id", "upd_time", "data_type", "data_id", "name", "addr", "addr_type", "digit_count", "data_limit", "his_cycle","rid", "digit_binary"};
 
 }
